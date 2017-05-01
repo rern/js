36e1e5b24584ebfbc24b26b0d61a79696a0f6e52
rern <rernrern@gmail.com>
2017-05-01 09:24:16 +0700

    Update sortable.js

--- sortable/sortable.js
+++ sortable/sortable.js
@@ -22,12 +22,13 @@ usage:
 $('tableid').sortable(); 		// without options > full page table
 // or
 $('tableid').sortable({
-	divBeforeTable: 'divbeforeid',	// default: (none) - div before table, enclosed in single div
-	divAfterTable: 'divafterid',	// default: (none) - div after table, enclosed in single div
-	initialSort: 'column#',		// default: (none) - start with 0
-	initialSortDesc: false,
-	locale: 'code',			// default: 'en' - locale code
-	negativeSort: [column#]		// default: (none) - column with negative value
+	divBeforeTable: 'divbeforeid'	// default: (none) - div before table, enclosed in single div
+	, divAfterTable: 'divafterid'	// default: (none) - div after table, enclosed in single div
+	, initialSort: 'column#'		// default: (none) - start with 0
+	. initialSortDesc: false	// default: false
+	, locale: 'code'			// default: 'en' - locale code
+	, negativeSort: [column#]		// default: (none) - column with negative value
+	, tableArray : []			// default: (none) - use table data array directly
 });
 ...
 
@@ -35,23 +36,21 @@ $('tableid').sortable({
   edit in `sortable.css`    
 */
 
-(function ($) {
+( function( $ ) {
 
-$.fn.sortable = function (options) {
+$.fn.sortable = function( options ) {
 //*****************************************************************************
-var settings = $.extend({ // defaults
-	divBeforeTable: '',
-	divAfterTable: '',
-	initialSort: '',
-	initialSortDesc: false,
-	locale: 'en',
-	negativeSort: []
+var settings = $.extend( { // defaults
+	divBeforeTable: ''
+	, divAfterTable: ''
+	, nitialSort: ''
+	, initialSortDesc: false
+	, locale: 'en'
+	, negativeSort: []
+	, tableArray : []
 }, options );
-var initialsort = settings.initialSort;
-var negativesort = settings.negativeSort;
-
 var shortvport = 414; // max height to apply fixed thead
-var timeout = 300; // less will break header and scroll position
+var timeout = 300; // try higher if table was not right
 
 var $window = $(window);
 var $table = this;
@@ -61,6 +60,28 @@ var $thtd = $thtr.children(); // either 'th' or 'td'
 var $tbody = $table.find('tbody');
 var $tbtr = $tbody.find('tr');
 var $tbtd = $tbtr.find('td');
+
+var initialsort = settings.initialSort;
+var negativesort = settings.negativeSort;
+// use table array directly if available
+if ( settings.tableArray.length ) {
+	var arr = settings.tableArray;
+} else {
+	// convert 'tbody' to value-only array [ [i, 'a', 'b', 'c'], [i, 'd', 'e', 'f'] ]
+	var arr = [];
+	$tbtr.each( function( i ) {
+		var tdarr = [ i ];
+		$(this).find('td').each( function( j ) {
+			if ( negativesort.indexOf( j ) === -1 ) {
+				var tdtxt = $(this).text();
+			} else { // minus value in column
+				var tdtxt = $(this).text().replace( /[^0-9\.\-]/g, '' ); // get only '0-9', '.' and '-'
+			}
+			tdarr.push( $thtd.eq( j ).text() == '' ? '' : tdtxt ); // blank header not sortable
+		} );
+		arr.push( tdarr );
+	} );
+}
 
 var divbeforeH = 0;
 var divafterH = 0;
@@ -74,21 +95,6 @@ if ( settings.divAfterTable ) {
 	divafterH = $( divafter ).outerHeight();
 	$( divafter ).addClass('divafter');
 }
-
-// convert 'tbody' to value-only array [ [i, 'a', 'b', 'c'], [i, 'd', 'e', 'f'] ]
-var arr = [];
-$tbtr.each( function(i) {
-	var tdarr = [i];
-	$(this).find('td').each( function(j) {
-		if ( negativesort.indexOf(j) === -1 ) {
-			var tdtxt = $(this).text();
-		} else { // minus value in column
-			var tdtxt = $(this).text().replace( /[^0-9\.\-]/g, '' ); // get only '0-9', '.' and '-'
-		}
-		tdarr.push( $thtd.eq(j).text() == '' ? '' : tdtxt ); // blank header not sortable
-	});
-	arr.push( tdarr );
-});
 
 // dynamic css - for divbeforeH underlay, divafterH and fixed thead2
 var tblid = this[0].id;
@@ -107,9 +113,9 @@ $('head').append('<style>'
 	+'.sortableth2 {top: '+ divbeforeH +'px;}\n'
 	+'#trlast {height: '+ ( divafterH + trH ) +'px;}\n'
 	+'@media(max-height: '+ shortvport +'px) {\n'
-		+'.divbefore, .divafter {position: relative;}'
-		+'.tblparent::before {position: absolute;}'
-		+'.tblparent::before, .sortableth2 {top: 0;}'
+		+'.divbefore {position: absolute;}'
+		+'.divafter {position: relative;}'
+		+'.sortableth2 {top: 0;}'
 		+'.sortable thead {visibility: visible;}'
 		+'#trlast {height: '+ trH +'px;}'
 	+'}'
@@ -123,29 +129,29 @@ function thead2align() {
 	setTimeout( function() { // wait rendering	
 		$thead2a.show(); // reset hidden
 		var thtdL = $thtd.length;
-		$thtd.each( function(i) {
+		$thtd.each( function( i ) {
 			if ( i > 0 && i < ( thtdL - 1 ) ) { // allocate width for sort icon to avoid clipped header
 				$(this)
 					.addClass('asctmp')
 					.css('min-width', $(this).outerWidth() +'px')
 					.removeClass('asctmp');
 			}
-			if ( $tbtd.eq(i).is(':visible') ) {
-				$thead2a.eq(i).css('width', $(this).outerWidth() +'px'); // include 'td' padding
+			if ( $tbtd.eq( i ).is(':visible') ) {
+				$thead2a.eq( i ).css('width', $(this).outerWidth() +'px'); // include 'td' padding
 			} else {
-				$thead2a.eq(i).hide(); // set hidden column
+				$thead2a.eq( i ).hide(); // set hidden column
 			}
-		});
-		$thead2a.eq(0).css( 'width', $tbtd.eq(0).outerWidth() ); // fix - tdpad reset to '0'
+		} );
+		$thead2a.eq( 0 ).css( 'width', $tbtd.eq( 0 ).outerWidth() ); // fix - tdpad reset to '0'
 		$thead2.show();
 	}, timeout );
 }
 
 // #2 - add fixed header for short viewport
 var th2html = '<a></a>'; // for 'td' click index
-$thtd.each( function(i) { // eq(i + 1) 'text-align' - compensate added tdpad
+$thtd.each( function( i ) { // eq(i + 1) 'text-align' - compensate added tdpad
 	th2html += '<a style="text-align: '+ $thtd.eq( i + 1 ).css('text-align') +';">'+ $(this).text() +'</a>';
-});
+} );
 $('body').prepend(
 	'<div id="'+ tblid +'th2" class="sortableth2" style="display: none">'+ th2html +'</div>'
 );
@@ -155,18 +161,16 @@ var $thead2a = $thead2.find('a');
 $thead2a.click( function() {
 	$thtd.eq( $(this).index() )
 		.click();
-});
+} );
 
 // #3 - add l/r padding 'td' to keep table center
 // 'detach' to avoid many dom traversings
 var $tbl = $table.detach();
-$tbl.find('tr').each( function() {
-	var tdpad = '<td class="tdpad"></td>';
-	$(this)
-		.prepend( tdpad )
-		.append( tdpad );
-});
-$(tblparent).append( $tbl );
+$thtr.prepend('<th class="tdpad"></th>')
+	.append('<th class="tdpad"></th>');
+$tbtr.prepend('<td class="tdpad"></td>')
+	.append('<td class="tdpad"></td>');
+$( tblparent ).append( $tbl );
 $thtd = $thtr.children(); // refresh cache after added
 $tbtd = $tbtr.find('td'); // refresh cache after added
 
@@ -186,44 +190,46 @@ thead2align();
 // reference for scrolling calculation
 var fromshortv = ( $window.height() <= shortvport ) ? 1 : 0;
 // get scroll position
+var scrl = [0, 0];
 var scrltop = 0;
 $window.scroll( function () {
-	scrltop = $window.scrollTop();
-//	console.log(scrltop);
-});
+	setTimeout( function() { // fix: delay to prevent  reset by screen rotation
+		scrltop = $window.scrollTop();
+	}, 500);
+} );
 
 // show top part on short viewport initial load
 $thtd = $thtr.children(); // with 'tdpad'
 setTimeout( function() {
-	$window.scrollTop(0);
+	$window.scrollTop( 0 );
 	initialsort && $thtd.eq( initialsort ).trigger( 'click', settings.initialSortDesc );
 }, timeout );
 
 // #8 - click 'thead' to sort
 $thtd.click( function( event, initdesc ) {
 	var i = $(this).index();
-	var numcol = negativesort.indexOf( i - 1 ); // '-1' - deduct 'tdpad' column
+	var numcol = negativesort.indexOf( i-1 ); // '-1' - deduct 'tdpad' column
 	var order = ( $(this).hasClass('asc') || initdesc ) ? 'desc' : 'asc';
 	// sort value-only array (multi-dimensional)
 	var sorted = arr.sort( function( a, b ) {
 		if ( order == 'desc') {
 			if ( numcol === -1 ) {
-				return a[i].localeCompare( b[i], settings.locale, { numeric: true } );
+				return a[ i ].localeCompare( b[ i ], settings.locale, { numeric: true } );
 			} else {
-				return a[i] - b[i];
+				return a[ i ] - b[ i ];
 			}
 		} else {
 			if ( numcol === -1 ) {
-				return b[i].localeCompare( a[i], settings.locale, { numeric: true } );
+				return b[ i ].localeCompare( a[ i ], settings.locale, { numeric: true } );
 			} else {
-				return b[i] - a[i];
+				return b[ i ] - a[ i ];
 			}
 		}
-	});
+	} );
 	// sort 'tbody' in-place by each 'array[0]', reference i [ [i, 'a', 'b', 'c'], [i, 'd', 'e', 'f'] ]
 	$.each( sorted, function() {
 		$tbody.prepend( $tbtr.eq( $(this)[0]) );
-	});
+	} );
 	// switch sort icon and highlight sorted column
 	$thead2a.add( $thtd ).add( $tbtd )
 		.removeClass('asc desc sorted');
@@ -231,32 +237,27 @@ $thtd.click( function( event, initdesc ) {
 		.addClass( order )
 			.add( $tbody.find('td:nth-child('+ ( i+1 ) +')') )
 			.addClass('sorted');
-});
+} );
 
 // #9 - screen rotate
-var scrltop0 = 0;
+var scrlcurrent = 0;
 window.addEventListener('orientationchange', function() {
-//		scrltop = $window.scrollTop(); // !!! detect incorrectly in fullscreen ios, chrome devtools
+//	scrltop = $window.scrollTop(); // !!! detect incorrectly in fullscreen ios, chrome devtools
 	// maintain scroll position on rotate
-//console.log('scrltop '+ scrltop);
-//console.log('fromshortv '+ fromshortv);
-//	if ($window.height() <= shortvport) {
 	if ( $('.sortableth2').css('top') == '0px' ) {
-		scrltop0 = scrltop + divbeforeH;
+		scrlcurrent = scrltop + divbeforeH;
 		fromshortv = 1;
 	} else {
-		scrltop0 = scrltop - ( fromshortv ? divbeforeH : 0 ); // less if from short viewport
-//		var scrltop0 = scrltop - (fromshortv ? divbeforeH + 243: 0); // '+243 for android
+		scrlcurrent = scrltop - ( fromshortv ? divbeforeH : 0 ); // omit only from short viewport
 		fromshortv = 0;
 	}
 	$thead2.hide();
 	thead2align(); // align thead2
-//console.log('scrltop0 '+ scrltop0);
 	
 	setTimeout( function() {
-		$window.scrollTop( scrltop0 );
+		$window.scrollTop( scrlcurrent );
 	}, timeout + 100);
-});
+} );
 //*****************************************************************************
 }
-} (jQuery));
+} ( jQuery ) );
